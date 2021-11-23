cd4e55ec7f9ab79d356190b3ff8f3f8ab8900e64
praisedpern <jamesajinks@gmail.com>
2021-11-23 11:11:38 +0000

    refactors UserContext into UserProvider custom provider

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -6,7 +6,7 @@ const UserCard = () => {
 
     return (
         <section class="App-user-card">
-            <img src="https://media.gq.com/photos/5582cbcb1177d66d68d5912d/master/pass/entertainment-2011-08-mustache-nick-offerman-mustache-96.jpg" />
+            <img alt='User avatar ' src="https://media.gq.com/photos/5582cbcb1177d66d68d5912d/master/pass/entertainment-2011-08-mustache-nick-offerman-mustache-96.jpg" />
             {currentUser}
         </section>
     )
--- src/contexts/User.js
+++ src/contexts/User.js
@@ -1,3 +1,13 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 export const UserContext = createContext()
+
+export const UserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState('light')
+
+    return (
+        <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
