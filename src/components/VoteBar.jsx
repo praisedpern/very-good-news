540db599742ad421e064149c5fc4a6c3191cbd4b
praisedpern <jamesajinks@gmail.com>
2021-11-24 14:57:50 +0000

    adds isEnabled patterns for voting buttons

--- src/components/VoteBar.jsx
+++ src/components/VoteBar.jsx
@@ -1,9 +1,11 @@
 import UserCard from './UserCard'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { patchVotes } from '../utils/apiPatch'
 
 const VoteBar = ({ props }) => {
     const [addedVotes, setAddedVotes] = useState(0)
+    const [plusEnabled, setPlusEnabled] = useState(true)
+    const [minusEnabled, setMinusEnabled] = useState(true)
 
     const handleVotes = (incByAmount) => {
         patchVotes(props.article_id, incByAmount)
@@ -12,11 +14,23 @@ const VoteBar = ({ props }) => {
         })
     }
 
+    useEffect(() => {
+        setPlusEnabled(() => {
+            if (addedVotes === 1) return false
+            else return true
+        })
+        setMinusEnabled(() => {
+            if (addedVotes === -1) return false
+            else return true
+        })
+    }, [addedVotes])
+
     return (
         <>
             <h3>{props.title}</h3>
             <section className="App-vote-bar">
                 <button
+                    disabled={!plusEnabled}
                     onClick={() => {
                         return handleVotes(1)
                     }}
@@ -25,6 +39,7 @@ const VoteBar = ({ props }) => {
                 </button>
                 votes: {props.votes + addedVotes}
                 <button
+                    disabled={!minusEnabled}
                     onClick={() => {
                         return handleVotes(-1)
                     }}
