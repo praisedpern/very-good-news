32bee261e9d0a68ab445dd0a565795c2c4cc3fb8
praisedpern <jamesajinks@gmail.com>
2021-11-25 18:03:22 +0000

    refactors sorter with radio buttons

--- src/components/Sorter.jsx
+++ src/components/Sorter.jsx
@@ -1,12 +1,15 @@
 const Sorter = () => {
     return (
-        <section className="App-sorter">Sort by<ul>
-            <li>created_at</li>
-            <li>votes</li>
-            <li>author</li>
-            <li>comment_count</li>
+        <section className="App-sorter">
+            Sort by
+            <input name="sort_by" value="created_at" type="radio" />
+            <label htmlFor="created_at">created_at</label>
+            <input name="sort_by" value="comment_count" type="radio" />
+            <label htmlFor="comment_count">comment_count</label>
+            <input name="sort_by" value="votes" type="radio" />
+            <label htmlFor="votes">votes </label>
             <button>Asc/Desc</button>
-            </ul></section>
+        </section>
     )
 }
 
