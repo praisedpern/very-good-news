e250f33f97f024200dd48a8ca1052bb362aaa4c6
praisedpern <jamesajinks@gmail.com>
2021-11-26 14:51:44 +0000

    moves rendering of radio buttons to a react state

--- src/components/Sorter.jsx
+++ src/components/Sorter.jsx
@@ -1,14 +1,60 @@
+import { useState } from 'react'
+
 const Sorter = () => {
+    const [radioObj, setRadioObj] = useState({
+        created_at: true,
+        comment_count: false,
+        votes: false,
+    })
+
+    const radioObjHelper = (propToChange) => {
+        setRadioObj((oldRadioObj) => {
+            const newRadioObj = { ...oldRadioObj }
+            for (const property in newRadioObj) {
+                newRadioObj[property] = false
+            }
+            newRadioObj[propToChange] = true
+            return newRadioObj
+        })
+    }
+
     return (
         <section className="App-sorter">
             Sort by
-            <input name="sort_by" value="created_at" type="radio" />
+            <input
+                name="sort_by"
+                value="created_at"
+                type="radio"
+                checked={radioObj.created_at}
+                onChange={() => {
+                    radioObjHelper('created_at')
+                }}
+            />
             <label htmlFor="created_at">created_at</label>
-            <input name="sort_by" value="comment_count" type="radio" />
+            <input
+                name="sort_by"
+                value="comment_count"
+                type="radio"
+                checked={radioObj.comment_count}
+                onChange={() => {
+                    radioObjHelper('comment_count')
+                }}
+            />
             <label htmlFor="comment_count">comment_count</label>
-            <input name="sort_by" value="votes" type="radio" />
+            <input
+                name="sort_by"
+                value="votes"
+                type="radio"
+                checked={radioObj.votes}
+                onChange={() => {
+                    radioObjHelper('votes')
+                }}
+            />
             <label htmlFor="votes">votes </label>
-            <button>Asc</button> <button>Desc</button>
+            <button onClick={()=>{
+                console.log(radioObj)
+            }
+            }>Asc</button> <button>Desc</button>
         </section>
     )
 }
