b28e91536e40da65333c1de0e7d94c03348c2545
praisedpern <jamesajinks@gmail.com>
2021-12-02 20:39:16 +0000

    adds UserCard to CommentCard

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,10 +1,14 @@
+import UserCard from './UserCard'
 import VoteBar from './VoteBar'
 
 const CommentCard = ({ comment }) => {
     return (
         <section className="App-comment-card">
             <VoteBar props={comment} />
+            <div className='App-article-body-container'>
             <p className="App-comment-body">{comment.body}</p>
+            <UserCard user={comment.author}/>
+            </div>
         </section>
     )
 }
